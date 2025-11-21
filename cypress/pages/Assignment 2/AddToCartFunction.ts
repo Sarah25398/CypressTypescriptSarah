9ee4e677d9ae49970856208a77fbd0d44b00b05d
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-21 16:15:03 +0700

    Enhance AddToCartFunction with new methods

--- cypress/pages/Assignment 2/AddToCartFunction.ts
+++ cypress/pages/Assignment 2/AddToCartFunction.ts
@@ -1,7 +1,7 @@
 import { BaseCommands } from "../../support/Command/BaseCommands";
 import { addToCartLocator } from "../../locators/Assignment 2/AddToCartLocator";
 import { addToCartFunctionData } from "../../data/DataTest/Assignement 2/AddToCartFunctionData";
-
+import { checkOutPageLocator } from "../../locators/Assignment 2/CheckoutPageLocator";
 
 export class AddToCartFunction {
     baseCommands: BaseCommands
@@ -14,11 +14,44 @@ export class AddToCartFunction {
     findTextProduct() {
         this.baseCommands.findElementByText(
             addToCartLocator.productNameTextLocator,
-            addToCartFunctionData.productNameTextLocator,
+            addToCartFunctionData.productNameText,
             true,
             true
         );
+    }
+    clickAddToCartButton() {
+        this.baseCommands.getElementByParentTag(
+            addToCartLocator.productNameTextLocator
+            , "Cucumber - 1 Kg"
+            , "div.product"
+            , addToCartLocator.addToCartButton
+        ).then((el) => {
+            cy.wrap(el).click({ force: true });
+        })
+    }
+    verifyAddedItem() {
 
     }
+    verifyClicKCartIcon() {
+        this.baseCommands.clickElement(
+            addToCartLocator.cartIcon
+        ).then(() => {
+            return cy.get("div.cart-preview.active").should("be.visible");
+        })
+    }
+    verifyItemAddedToCart() {
+        this.baseCommands.getElementText(
+            addToCartLocator.productItemAddedToCartLocator
+        ).should("eq", addToCartFunctionData.productNameText);
+    }
+    clickProceedCheckoutButton() {
+        this.baseCommands.verifyEnableAndClick(
+            cy.get('button[type="button"]')
+                .contains("PROCEED TO CHECKOUT")).then(() => {
+                    cy.get(checkOutPageLocator.productDetailTable).should("be.visible");
+                });
+
+    }
+
 
 }
