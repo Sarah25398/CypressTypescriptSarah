337ab9e3cbe1e2519cfa4fbec099ed81209a2d2a
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-21 16:12:46 +0700

    Enhance AddToCartFunction tests with checkout scenarios
    Refactor AddToCartFunction tests to include checkout flow and multiple promo code application.

--- cypress/e2e/Assignment 2/AddToCartFunction.cy.ts
+++ cypress/e2e/Assignment 2/AddToCartFunction.cy.ts
@@ -1,20 +1,47 @@
 import { AddToCartFunction } from "../../pages/Assignment 2/AddToCartFunction";
 import { BaseCommands } from "../../support/Command/BaseCommands";
+import { CheckoutPage } from "../../pages/Assignment 2/CheckoutPage";
+import { addToCartFunctionData } from "../../data/DataTest/Assignement 2/AddToCartFunctionData";
 
 describe('Add to Cart Functionality', () => {
 
     let addToCartFunction: AddToCartFunction;
     let baseCommands: BaseCommands;
+    let checkoutPage: CheckoutPage;
     beforeEach(() => {
+        checkoutPage = new CheckoutPage();
         baseCommands = new BaseCommands();
         addToCartFunction = new AddToCartFunction();
         addToCartFunction.navigateToProductPage()
     });
-    it('Verify go to correct page', () => {
-        addToCartFunction.navigateToProductPage();
-
-    });
     it('Verify product name', () => {
         addToCartFunction.findTextProduct();
+    });
+    it('Add to Cart', () => {
+        addToCartFunction.clickAddToCartButton();
+    })
+    it('Verify Added Item with trial step ', () => {
+        addToCartFunction.clickAddToCartButton();
+        addToCartFunction.verifyClicKCartIcon();
+        addToCartFunction.verifyItemAddedToCart();
+        addToCartFunction.clickProceedCheckoutButton();
+        checkoutPage.enterPromoteCode("23123");
+        checkoutPage.applyPromoteCode();
+        checkoutPage.verifyPromoteInfo("Invalid code ..!");
+        checkoutPage.verifyDiscountNumber("0%");
+    })
+    it.only('Verify Apply Multiple codes and E2E flow ', () => {
+        addToCartFunction.clickAddToCartButton();
+        addToCartFunction.verifyClicKCartIcon();
+        addToCartFunction.verifyItemAddedToCart();
+        addToCartFunction.clickProceedCheckoutButton();
+        checkoutPage.applyMultiplePromoteCode();
+        checkoutPage.clickPlaceOrderButton();
+        checkoutPage.selectCountryData("Vietnam");
+        checkoutPage.checkConditionBox();
+        checkoutPage.pressProceedCheckoutButton();
+        checkoutPage
+            .getSucessCheckOutText(addToCartFunctionData.messageCheckOut1
+                , addToCartFunctionData.messageCheckOut2);
     })
 });
