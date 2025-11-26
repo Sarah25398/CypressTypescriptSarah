2fdf433ea8d5d13a363c54d59c07232f7403fa15
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-26 15:27:16 +0700

    Update PandaMobile.cy.ts

--- cypress/e2e/Assignment 3/PandaMobile.cy.ts
+++ cypress/e2e/Assignment 3/PandaMobile.cy.ts
@@ -2,12 +2,15 @@ import { PandaHomePage } from "../../pages/Assignment 3/PandaHomePage";
 import {
     BaseCommands
 } from "../../support/Command/BaseCommands";
+import { AddressBookPage } from "../../pages/Assignment 3/AddressBookPage";
 
 describe("Panda Mobile", () => {
+    let addressBookPage: AddressBookPage;
     let baseCommands: BaseCommands;
     let pandaHomePage: PandaHomePage;
 
     beforeEach(() => {
+        addressBookPage = new AddressBookPage();
         baseCommands = new BaseCommands();
         pandaHomePage = new PandaHomePage();
         pandaHomePage.navigateToPandaHomePage();
@@ -19,5 +22,15 @@ describe("Panda Mobile", () => {
         pandaHomePage.getPageTitle().then((title) => {
             expect(title).to.equal("Login or Create an Account");
         })
+        pandaHomePage.clickCreateAccount();
+        pandaHomePage.fillNameField();
+        pandaHomePage.fillAllFields();
+        pandaHomePage.clickRegisterButton();
+        pandaHomePage.verifyRegistration();
+        addressBookPage.clickAddressBook();
+        addressBookPage.fillAddressRegister();
+        addressBookPage.selectState();
+        addressBookPage.clickAdressRegister();
+
     });
 });
