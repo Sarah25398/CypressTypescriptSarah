d453a66cc26736380eda388f87079b4472c1ece9
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-26 11:21:20 +0700

    Create UrlEnum.ts

---
+++ cypress/data/Enum/UrlEnum.ts
@@ -0,0 +1,19 @@
+export enum URL {
+    add_to_cart_url = "https://rahulshettyacademy.com/seleniumPractise/#/", // assignment 2
+    tech_panda_url = "https://live.techpanda.org/index.php/mobile.html" // assigment 3 
+}
+export const isUrl = (url: string): boolean => {
+    return Object.values(URL).includes(url as URL);
+}
+export const isURLValid = (url: string): boolean => {
+    return url in URL;
+}
+export const getURL = (url: string): URL => {
+    return URL[url as keyof typeof URL];
+}
+export const validateURL = (url: string): URL => {
+    if (!isURLValid(url)) {
+        throw new Error(`Url is not in enum list: ${url}`);
+    }
+    return getURL(url);
+}
