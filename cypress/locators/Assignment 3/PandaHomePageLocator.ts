d82d8387f0e2ed31675421200e33bb1bdc197e1d
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-26 15:27:45 +0700

    Update PandaHomePageLocator.ts

--- cypress/locators/Assignment 3/PandaHomePageLocator.ts
+++ cypress/locators/Assignment 3/PandaHomePageLocator.ts
@@ -1,5 +1,16 @@
 export const PandaHomePageLocator = {
     accountIcon: 'span.label',
     loginButton: 'a[title="Log In"]',
-    createAccountButton: '[title="Create an Account"].button'
+    createAccountButton: '[title="Create an Account"].button',
+    firstName: '#firstname',
+    middleName: '#middlename',
+    lastName: '#lastname',
+    email: '#email_address',
+    password: '#password',
+    confirmPassword: '#confirmation',
+    registerButton: 'button[title="Register"]',
+    welcomeMessage: '.dashboard .welcome-msg',
+
+
+
 }
