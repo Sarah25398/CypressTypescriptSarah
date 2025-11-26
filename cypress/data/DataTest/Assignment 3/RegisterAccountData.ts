cbee68e55bd7f6af4a4a28e365915c0f80bfd365
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-26 15:26:39 +0700

    Update RegisterAccountData.ts

--- cypress/data/DataTest/Assignment 3/RegisterAccountData.ts
+++ cypress/data/DataTest/Assignment 3/RegisterAccountData.ts
@@ -1,1 +1,47 @@
 
+
+
+import { generateEmail } from "../../../utilities/Data_Generate_Random";
+
+export interface RegisterAccountData {
+    firstName: string;
+    lastName: string;
+    middleName: string;
+    email: string;
+    password: string;
+}
+export interface AddressBookData {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    company: string;
+    phone: number;
+    fax: string;
+    street_1: string;
+    street_2: string;
+    city: string;
+    zip: string;
+    state: string,
+    country: string
+}
+export const addressBookData: AddressBookData = {
+    firstName: "ahmed",
+    middleName: "abd elhamed",
+    lastName: "mohamed",
+    company: "company",
+    phone: 123456,
+    fax: "123456",
+    street_1: "street 1",
+    street_2: "street 2",
+    city: "city",
+    zip: "123456",
+    state: "Alabama",
+    country: ""
+}
+export const registerAccountData: RegisterAccountData = {
+    firstName: "ahmed",
+    lastName: "abd elhamed",
+    middleName: "mohamed",
+    email: generateEmail(),
+    password: "123456"
+}
