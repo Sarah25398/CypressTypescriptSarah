b8e006f67fa8281b1c6398647f591e1d8f7e2252
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-11-27 11:23:51 +0700

    Update CustomAssertions.ts

--- cypress/support/Assertions/CustomAssertions.ts
+++ cypress/support/Assertions/CustomAssertions.ts
@@ -1,8 +1,25 @@
-export function assertEqual<T>(actualValue : T, expectedValue: T, message: string) : void  { 
-    expect(actualValue).to.equal(expectedValue, message);
-    
-} 
-export function assertNotEqual<T>(actualValue : T, expectedValue: T, message: string) : void  { 
-    expect(actualValue).not.to.equal(expectedValue, message);
-    
+export function assertEqual<T>(expected: T, actual: T, message: string): void {
+    expect(actual).to.equal(expected, message);
+}
+export function assertStringIncludes(actual: string, expected: string, message: string): void {
+    expect(actual).to.include(expected, message);
+}
+export function assertNotEqual<T>(actual: T, expected: T, message: string): void {
+    expect(actual).not.equal(expected, message);
+}
+export function assertDeepEqual<T>(actual: T, expected: T, message: string): void {
+    expect(actual).to.deep.equal(expected, message);
+}
+export function assertInArray<T>(actual: T[], expected: T, message: string): void {
+    expect(actual).to.include(expected, message);
+}
+export function assertNotInArray<T>(actual: T[], expected: T, message: string): void {
+    expect(actual).not.to.include(expected, message);
+}
+export function assertNumber(actual: number, expected: number, message: string): boolean {
+    const isCompare = (actual >= expected) ? true : false;
+    return isCompare;
+}
+export function assertMatch(actual: string, expected: RegExp, message: string): void {
+    expect(actual).to.match(expected, message);
 }
