fe49b03e57df5b4bdbf15fe8dbafa0d30ebee20a
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-12-03 16:20:55 +0700

    Update and rename add.ts to livePandaLoctor.ts

---
+++ cypress/locators/Assignment_5/livePandaLoctor.ts
@@ -0,0 +1,11 @@
+export const livePandaLocators = {
+    livePandaLogo: '.logo img',
+    loginElement: '#header-account li a',
+    loginText: '.page-title',
+    createAccountButton: '.buttons-set .button',
+    createAccountText: '.page-title h1',
+    requiredField: '.fieldset .required',
+    sortByElement: '.toolbar-bottom select[title="Sort By"]'
+
+
+}
