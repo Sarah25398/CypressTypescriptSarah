3d047cd5b83df72f723139956c3881e518923ec7
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-12-05 16:26:11 +0700

    Update and rename Add.ts to ProductPage.ts

---
+++ cypress/locators/Assignment_6/ProductPage.ts
@@ -0,0 +1,12 @@
+
+export const ProductLocator = {
+    productTitle: 'h4.product-name',
+    addToCartButton: '[type="button"]',
+    cartIcon: "div .cart-icon",
+    productItemAddedToCartLocator: ".cart-preview.active .product-name",
+    productDetailTable: 'table.cartTable#productCartTables',
+
+
+
+
+}
