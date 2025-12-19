3b9f587ae0bba026e873bc9e6fad6d1895400a3e
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-12-19 13:54:32 +0700

    Add my_account_url to URL enum

--- cypress/data/Enum/UrlEnum.ts
+++ cypress/data/Enum/UrlEnum.ts
@@ -1,7 +1,8 @@
 export enum URL {
     add_to_cart_url = "https://rahulshettyacademy.com/seleniumPractise/#/", // assignment 2
     tech_panda_url = "https://live.techpanda.org/index.php/mobile.html", // assigment 3
-    iframe_url = 'https://iframetester.com/'
+    iframe_url = 'https://iframetester.com/',
+    my_account_url = 'https://practice.automationtesting.in/'
 }
 export const isUrl = (url: string): boolean => {
     return Object.values(URL).includes(url as URL);
