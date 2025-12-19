e8d50ae0e5a3da7b595971648f2b4963deca61c3
Sarah25398 <128563759+Sarah25398@users.noreply.github.com>
2025-12-19 15:14:11 +0700

    Enhance MyAccount test with register function verification
    Added verification for the register function in the MyAccount test case.

--- cypress/e2e/Assignment_8/MyAccount.cy.ts
+++ cypress/e2e/Assignment_8/MyAccount.cy.ts
@@ -14,6 +14,7 @@ describe('Assignment 8', () => {
 
     it('Verify register login test case', () => {
         myAccountPage.verifyMyAccountNavigate();
+        myAccountPage.verifyRegisterFunction();
 
     })
 })
